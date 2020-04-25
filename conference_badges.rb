f060482c93fd509637ee30271db843fb29033abb
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-25 21:23:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,6 @@ end
 
 badge_maker("Arel")
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace",
-"Linus", "Matz"]
-  
 def batch_badge_creator(names)
   badges = []
   names.each { |name| badges << 
